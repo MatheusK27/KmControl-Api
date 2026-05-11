a209090810505bf9425b2d92b135bd30c5af84f5
MatheusK27 <matheuskleinm@hotmail.com>
2026-05-11 07:27:45 -0300

    Implementacao usuario

---
+++ src/main/java/com/matheus/entidade/Usuario.java
@@ -0,0 +1,38 @@
+package com.matheus.entidade;
+
+
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import java.time.LocalDateTime;
+
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+@Entity
+@Table(name ="usuarios")
+public class Usuario {
+
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private int id;
+
+    @Column(nullable = false)
+    private String nome;
+
+    @Column(nullable = false, unique = true)
+    private String login;
+
+    @Column(nullable = false)
+    private String senha;
+
+    
+    private String perfil = "OPERADOR";
+    private boolean ativo = true;
+
+    @Column(name = "criado_em", nullable = false, updatable = false)
+    private LocalDateTime criadoEm =  LocalDateTime.now();
+}

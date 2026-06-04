1ad75a9d144d182cdbc899c49a3b843c5f1a4624
MatheusK27 <matheuskleinm@hotmail.com>
2026-06-04 17:16:38 -0300

    Ajuste em entidade Registro Km Teste

--- src/test/java/com/matheus/entidades/RegistroKmEntidadeTest.java
+++ src/test/java/com/matheus/entidades/RegistroKmEntidadeTest.java
@@ -20,61 +20,81 @@ import static org.mockito.Mockito.mock;
 public class RegistroKmEntidadeTest {
 
     @Test
-    void deveLancarErroQuandoKmEntradaForNull(){
-        var dados= new DadosCadastroRegistroKm(1L, LocalDate.now(),null,null,null,null);
+    void deveLancarErroQuandoKmEntradaForNull() {
+        var dados = new DadosCadastroRegistroKm(1L, LocalDate.now(), null, null, null, null);
+        var motoboy = mock(Motoboy.class);
+        var erro = assertThrows(RegraDeNegocioException.class, () -> {
+            new RegistroKm(dados, motoboy);
+        });
+        assertEquals("Km entrada não fornecido ou menor que zero", erro.getMessage());
+
+    }
+
+    @Test
+    void deveLancarErroQuandoKmEntradaForZero() {
+        var dados = new DadosCadastroRegistroKm(1L, LocalDate.now(), 0, null, null, null);
+        var motoboy = mock(Motoboy.class);
+        var erro =  assertThrows(RegraDeNegocioException.class, () -> {
+            new RegistroKm(dados, motoboy);
+        });
+        assertEquals("Km entrada não fornecido ou menor que zero", erro.getMessage());
+
+    }
+
+    @Test
+    void deveLancarErroQuandoDataNaoForDataAtual(){
+        var dados = new DadosCadastroRegistroKm(1L, LocalDate.now().plusDays(1), 1000, 1100, 1200, 1300);
         var motoboy= mock(Motoboy.class);
-        assertThrows(RegraDeNegocioException.class, () -> {new RegistroKm(dados,motoboy);});
-    }
-
-    @Test
-    void deveLancarErroQuandoKmEntradaForZero(){
-        var dados= new DadosCadastroRegistroKm(1L, LocalDate.now(),0,null,null,null);
-        var motoboy= mock(Motoboy.class);
-        assertThrows(RegraDeNegocioException.class, () -> {new RegistroKm(dados,motoboy);});
-    }
-
-    @Test
-    void deveLancarErroQuandoKmEntradaForNullValidarRegistroCompleto(){
-       var dados= new DadosAtualizarRegistroKm(1L,1L, LocalDate.now(),null,1100,1100,1200);
-       var registro= new RegistroKm();
-        assertThrows(RegraDeNegocioException.class,()-> registro.atualizarKm(dados) );
-    }
-
-    @Test
-    void deveLancarErroQuandoKmSaidaAlmocoforMenorQueKmDeEntrada(){
-        var dados= new DadosAtualizarRegistroKm(1L,1L, LocalDate.now(),1000,900,1100,1200);
-       var registro= new RegistroKm();
-       var erro= assertThrows(RegraDeNegocioException.class,()-> registro.atualizarKm(dados) );
-       assertEquals("KM de saída para almoço não pode ser menor que KM de entrada", erro.getMessage());
+        var erro = assertThrows(RegraDeNegocioException.class, () -> new RegistroKm(dados, motoboy));
+        assertEquals( "Permitido somente data atual!", erro.getMessage());
 
     }
 
     @Test
-    void deveLancarErroQuandoKmSaindaAlmocoForNullERetornoNaoForNull(){
-        var dados= new DadosAtualizarRegistroKm(1L,1L, LocalDate.now(),1000,null,1000,1200);
-        var registro= new RegistroKm();
-        var erro = assertThrows(RegraDeNegocioException.class,()-> registro.atualizarKm(dados) );
+    void deveLancarErroQuandoKmEntradaForNullValidarRegistroCompleto() {
+        var dados = new DadosAtualizarRegistroKm(1L, 1L, LocalDate.now(), null, 1100, 1100, 1200);
+        var registro = new RegistroKm();
+        var erro = assertThrows(RegraDeNegocioException.class, () -> registro.atualizarKm(dados));
+        assertEquals( "KM de entrada é obrigatório", erro.getMessage());
+
+    }
+
+    @Test
+    void deveLancarErroQuandoKmSaidaAlmocoforMenorQueKmDeEntrada() {
+        var dados = new DadosAtualizarRegistroKm(1L, 1L, LocalDate.now(), 1000, 900, 1100, 1200);
+        var registro = new RegistroKm();
+        var erro = assertThrows(RegraDeNegocioException.class, () -> registro.atualizarKm(dados));
+        assertEquals("KM de saída para almoço não pode ser menor que KM de entrada", erro.getMessage());
+
+    }
+
+    @Test
+    void deveLancarErroQuandoKmSaindaAlmocoForNullERetornoNaoForNull() {
+        var dados = new DadosAtualizarRegistroKm(1L, 1L, LocalDate.now(), 1000, null, 1000, 1200);
+        var registro = new RegistroKm();
+        var erro = assertThrows(RegraDeNegocioException.class, () -> registro.atualizarKm(dados));
         assertEquals("KM de retorno do almoço exige KM de saída para almoço", erro.getMessage());
     }
 
 
     @Test
-    void deveLancarErroQuandoKmRetornoAlmocoForMenorQueKmDeSaida(){
-        var dados= new DadosAtualizarRegistroKm(1L,1L, LocalDate.now(),1000,1100,1000,1200);
-        var registro= new RegistroKm();
-        var erro = assertThrows(RegraDeNegocioException.class,()-> registro.atualizarKm(dados) );
+    void deveLancarErroQuandoKmRetornoAlmocoForMenorQueKmDeSaida() {
+        var dados = new DadosAtualizarRegistroKm(1L, 1L, LocalDate.now(), 1000, 1100, 1000, 1200);
+        var registro = new RegistroKm();
+        var erro = assertThrows(RegraDeNegocioException.class, () -> registro.atualizarKm(dados));
         assertEquals("KM de retorno do almoço não pode ser menor que KM de saída para almoço", erro.getMessage());
     }
 
     @Test
-    void deveLancarErroQuandoKmfinalForMenorQueKmAnterior(){
-        var dados= new DadosAtualizarRegistroKm(1L,1L, LocalDate.now(),1100,1200,1200,1150);
-        var registro= new RegistroKm();
-        var erro = assertThrows(RegraDeNegocioException.class,()-> registro.atualizarKm(dados));
+    void deveLancarErroQuandoKmfinalForMenorQueKmAnterior() {
+        var dados = new DadosAtualizarRegistroKm(1L, 1L, LocalDate.now(), 1100, 1200, 1200, 1150);
+        var registro = new RegistroKm();
+        var erro = assertThrows(RegraDeNegocioException.class, () -> registro.atualizarKm(dados));
         assertEquals("KM final não pode ser menor que o KM anterior", erro.getMessage());
 
     }
-    }
+
+}
 
 
 

9526b9b8a917d2dc8c0ded8d2f44d2868a75c9e5
MatheusK27 <matheuskleinm@hotmail.com>
2026-07-17 20:22:26 -0300

    ajuste entidade RegistroKm

--- src/main/java/com/matheus/entidades/entidades/RegistroKm.java
+++ src/main/java/com/matheus/entidades/entidades/RegistroKm.java
@@ -6,14 +6,13 @@ import com.matheus.entidades.dtoEntrada.DadosCadastroRegistroKm;
 import com.matheus.entidades.dtoEntrada.DadosFinalizarCadastroRegistroKM;
 import com.matheus.infra.RegraDeNegocioException;
 import jakarta.persistence.*;
-import lombok.AllArgsConstructor;
-import lombok.Data;
-import lombok.NoArgsConstructor;
+import lombok.*;
 
 import java.time.LocalDate;
 import java.time.LocalDateTime;
 
-@Data
+@Getter
+@Setter
 @AllArgsConstructor
 @NoArgsConstructor
 @Entity
